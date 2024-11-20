c0c91cdd7d48d68e985ce0b8b5256074fbda186d
Walter Rocha <walterrochasevilla@gmail.com>
2024-11-20 15:17:38 -0400

    Refactor: extraer lógica de delimitador personalizado a una función

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,26 +1,33 @@
+function obtenerDelimitador(inputString) {
+  const delimitadorPorDefecto = /,|-/;
+
+  const delimitadorPersonalizado = inputString.match(/^\/\/(\[.+?\])+\s/);
+  if (!delimitadorPersonalizado) {
+    return { delimitador: delimitadorPorDefecto, numerosCadena: inputString };
+  }
+
+  const delimitadores = [...inputString.matchAll(/\[(.+?)\]/g)].map(match => match[1]);
+  const delimitadoresEscapados = delimitadores.map(delim => delim.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));
+
+  const delimitador = new RegExp(`${delimitadoresEscapados.join('|')}|,|-`);
+  const numerosCadena = inputString.slice(delimitadorPersonalizado[0].length);
+
+  return { delimitador, numerosCadena };
+}
+
 function calcular(inputString) {
   if (inputString.trim() === "") {
     return 0;
-  } else {
-    let delimitador = /,|-/;
-    let numerosCadena = inputString;
-
-    const delimitadorPersonalizado = inputString.match(/^\/\/(\[.+?\])+\s/);
-    if (delimitadorPersonalizado) {
-      const delimitadores = [...inputString.matchAll(/\[(.+?)\]/g)].map(match => match[1]);
-
-      const delimitadoresEscapados = delimitadores.map(delim => delim.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));
-      delimitador = new RegExp(`${delimitadoresEscapados.join('|')}|,|-`);
-      numerosCadena = inputString.slice(delimitadorPersonalizado[0].length);
-    }
-
-    const numeros = numerosCadena
-      .split(delimitador)
-      .map(Number)
-      .filter(num => num <= 1000);
-
-    return numeros.reduce((acc, num) => acc + num, 0);
   }
+
+  const { delimitador, numerosCadena } = obtenerDelimitador(inputString);
+
+  const numeros = numerosCadena
+    .split(delimitador)
+    .map(Number)
+    .filter(num => num <= 1000);
+
+  return numeros.reduce((acc, num) => acc + num, 0);
 }
 
 export default calcular;
